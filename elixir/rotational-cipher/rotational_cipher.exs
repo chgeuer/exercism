e254c18b794d3530f0e19669f0347d0212741cde
Christian Geuer-Pollmann <chgeuer@microsoft.com>
2017-06-11 21:01:06 +0200

    .

--- elixir/rotational-cipher/rotational_cipher.exs
+++ elixir/rotational-cipher/rotational_cipher.exs
@@ -8,6 +8,22 @@ defmodule RotationalCipher do
   """
   @spec rotate(text :: String.t(), shift :: integer) :: String.t()
   def rotate(text, shift) do
+    text 
+    |> String.to_charlist
+    |> Enum.map(&(transform(&1, shift)))
+    |> to_string
   end
+
+  defp transform(c, delta) when ?A <= c and c <= ?Z do
+    i = rem((c - ?A) + delta, 26)
+    if i < 0, do: i + ?A + 26, else: i + ?A
+  end
+  
+  defp transform(c, delta) when ?a <= c and c <= ?z do
+    i = rem((c - ?a) + delta, 26)
+    if i < 0, do: i + ?a + 26, else: i + ?a
+  end
+
+  defp transform(c, _delta), do: c
 end
 
