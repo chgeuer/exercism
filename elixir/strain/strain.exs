30452f14abe9f4868fe5187a9db804ed2e621e7e
Christian Geuer-Pollmann <chgeuer@microsoft.com>
2017-06-11 22:16:58 +0200

    .

--- elixir/strain/strain.exs
+++ elixir/strain/strain.exs
@@ -1,21 +1,21 @@
 defmodule Strain do
-  @doc """
-  Given a `list` of items and a function `fun`, return the list of items where
-  `fun` returns true.
 
-  Do not use `Enum.filter`.
-  """
   @spec keep(list :: list(any), fun :: ((any) -> boolean)) :: list(any)
   def keep(list, fun) do
+    enum_filter(list, fun)
   end
 
-  @doc """
-  Given a `list` of items and a function `fun`, return the list of items where
-  `fun` returns false.
+  @spec keep(list :: list(any), fun :: ((any) -> boolean)) :: list(any)
+  defp enum_filter([ head | tail ], fun) do
+    case fun.(head) do
+      true  -> [ head | enum_filter(tail, fun) ]
+      false -> enum_filter(tail, fun)
+    end
+  end
+  defp enum_filter([], _fun), do: []
 
-  Do not use `Enum.reject`.
-  """
   @spec discard(list :: list(any), fun :: ((any) -> boolean)) :: list(any)
   def discard(list, fun) do
+    enum_filter(list, fn(x) -> !fun.(x) end)
   end
 end
