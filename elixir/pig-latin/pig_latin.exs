f249f3df1348e9df170fa9224caa103fa5f50c7f
Christian Geuer-Pollmann <chgeuer@microsoft.com>
2017-06-14 13:21:47 +0200

    .

--- elixir/pig-latin/pig_latin.exs
+++ elixir/pig-latin/pig_latin.exs
@@ -15,6 +15,26 @@ defmodule PigLatin do
   """
   @spec translate(phrase :: String.t()) :: String.t()
   def translate(phrase) do
+    phrase
+    |> String.split(" ", trim: true)
+    |> Enum.map(&(translate_word(&1)))
+    |> Enum.join(" ")
   end
-end
 
+  @spec translate_word(phrase :: String.t()) :: String.t()
+  defp translate_word(phrase) do
+    cond do
+      phrase |> String.match?(~r/^(thr|sch)/i) -> phrase |> rule3
+      phrase |> String.match?(~r/^(ch|qu|th)/i) -> phrase |> rule2
+      phrase |> String.match?(~r/^(yt|xr)/i) -> phrase |> rule0
+      phrase |> String.match?(~r/^([bcdfghjklmnpqrstvwxyz])/i) -> phrase |> rule1
+      phrase |> String.match?(~r/^([aeiou])/i) -> phrase |> rule0
+      true -> "misc"
+    end
+  end
+
+  defp rule0(phrase), do: "#{phrase}ay"
+  defp rule1(<< c1 :: utf8, rest :: binary >>), do: "#{ rest |> to_string }#{ << c1 :: utf8 >>}ay"
+  defp rule2(<< c1 :: utf8, c2 :: utf8, rest :: binary >>), do: "#{ rest |> to_string }#{ << c1 :: utf8, c2 :: utf8 >>}ay"
+  defp rule3(<< c1 :: utf8, c2 :: utf8, c3 :: utf8, rest :: binary >>), do: "#{ rest |> to_string }#{ << c1 :: utf8, c2 :: utf8, c3 :: utf8 >>}ay"
+end
