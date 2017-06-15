908194285edd2b6a58db71e26f2526f927b3c40c
Christian Geuer-Pollmann <chgeuer@microsoft.com>
2017-06-15 22:51:14 +0200

    .

--- elixir/bob/bob.exs
+++ elixir/bob/bob.exs
@@ -1,8 +1,17 @@
 defmodule Bob do
   def hey(input) do
+    input = input |> String.trim
     cond do
-        true -> raise "Your implementation goes here"
-
+      input |> isEmpty -> "Fine. Be that way!"
+      input |> isQuestion -> "Sure."
+      input |> isShouting -> "Whoa, chill out!"
+      true -> "Whatever."
     end
   end
+
+  def isEmpty(input), do: input |> String.equivalent?("")
+  def isQuestion(input), do: input |> String.ends_with?("?")
+  def isUppercase(input), do: input |> String.upcase == input
+  def isContainsChars(input), do: not (input |> String.match?(~r/^[\s01234567890,.-]*$/))
+  def isShouting(input), do: isUppercase(input) && isContainsChars(input) 
 end
