680f5159e77c7c14f4d49529edd9fac085c00e80
Christian Geuer-Pollmann <chgeuer@microsoft.com>
2017-06-20 08:50:47 +0200

    a

--- elixir/raindrops/raindrops.exs
+++ elixir/raindrops/raindrops.exs
@@ -10,15 +10,11 @@ defmodule Raindrops do
   """
   @spec convert(pos_integer) :: String.t
   def convert(number) do
-    case %{ :three => 0 == rem(number, 3), :five => 0 == rem(number, 5), :seven => 0 == rem(number, 7) } do
-      %{ :three => false, :five => false, :seven => false } -> number |> to_string         
-      %{ :three => true, :five => false, :seven => false } -> "Pling"         
-      %{ :three => false, :five => true, :seven => false } -> "Plang"         
-      %{ :three => false, :five => false, :seven => true } -> "Plong"         
-      %{ :three => true, :five => true, :seven => false } -> "PlingPlang"         
-      %{ :three => true, :five => false, :seven => true } -> "PlingPlong"         
-      %{ :three => false, :five => true, :seven => true } -> "PlangPlong"         
-      %{ :three => true, :five => true, :seven => true } -> "PlingPlangPlong"         
+    results = %{ :three => 0 == rem(number, 3), :five => 0 == rem(number, 5), :seven => 0 == rem(number, 7) }
+    plingplangplong = "#{if results.three, do: "Pling"}#{if results.five, do: "Plang"}#{if results.seven, do: "Plong"}"
+    case plingplangplong do
+      "" -> number |> to_string
+      _ -> plingplangplong
     end
   end
 end
