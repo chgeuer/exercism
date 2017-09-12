ad1257fd0cd260fe87ea738ff82b1a1d94c2d3d8
Christian Geuer-Pollmann <chgeuer@microsoft.com>
2017-09-13 00:49:23 +0100

    .

--- go/two-fer/two_fer.go
+++ go/two-fer/two_fer.go
@@ -1,19 +1,15 @@
-// This is a "stub" file.  It's a little start on your solution.
-// It's not a complete solution though; you have to write some code.
-
-// Package twofer should have a package comment that summarizes what it's about.
-// https://golang.org/doc/effective_go.html#commentary
+// Package twofer solves a coding exercise.
 package twofer
 
 import (
 	"fmt"
 )
 
-// ShareWith needs a comment documenting it.
+// ShareWith returns a computed string.
 func ShareWith(name string) string {
 	if len(name) == 0 {
 		name = "you"
-	} 
+	}
 
 	return fmt.Sprintf("One for %s, one for me.", name)
 }
