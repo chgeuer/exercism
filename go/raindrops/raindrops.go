634ab27b331e983a3398cbb41eea336f2820b389
Christian Geuer-Pollmann <chgeuer@microsoft.com>
2017-09-15 16:08:00 +0200

    raindrops.go

---
+++ go/raindrops/raindrops.go
@@ -0,0 +1,28 @@
+package raindrops
+
+import (
+	"strconv"
+	"strings"
+)
+
+const testVersion = 3
+
+func Convert(n int) string {
+	sli := make([]string, 3)
+	if n%3 == 0 {
+		sli = append(sli, "Pling")
+	}
+	if n%5 == 0 {
+		sli = append(sli, "Plang")
+	}
+	if n%7 == 0 {
+		sli = append(sli, "Plong")
+	}
+	plingPlangPlong := strings.Join(sli[:], "")
+
+	if plingPlangPlong != "" {
+		return plingPlangPlong
+	} else {
+		return strconv.Itoa(n)
+	}
+}
