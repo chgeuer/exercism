af74e45e79a6da19783fdc6c5b4eaacae61d3022
Christian Geuer-Pollmann <chgeuer@microsoft.com>
2017-09-15 15:38:25 +0200

    Added hamming.go

---
+++ go/hamming/hamming.go
@@ -0,0 +1,24 @@
+package hamming
+
+import (
+	"errors"
+)
+
+const testVersion = 6
+
+func Distance(a, b string) (int, error) {
+	if len(a) != len(b) {
+		return -1, errors.New("DNA sequences don't have same size")
+	}
+
+	ra := []rune(a)
+	rb := []rune(b)
+	hammingWeight := 0
+	for i := 0; i < len(ra); i++ {
+		if ra[i] != rb[i] {
+			hammingWeight++
+		}
+	}
+
+	return hammingWeight, nil
+}
