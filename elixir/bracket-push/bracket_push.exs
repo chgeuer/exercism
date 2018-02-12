a87218470cbd7ddabc94c82ec531729f21954dfc
Christian Geuer-Pollmann <christian.geuer-pollmann@web.de>
2018-02-12 19:17:55 +0100

    bracket-push

---
+++ elixir/bracket-push/bracket_push.exs
@@ -0,0 +1,20 @@
+defmodule BracketPush do
+  @doc """
+  Checks that all the brackets and braces in the string are matched correctly, and nested correctly
+  """
+  @spec check_brackets(String.t()) :: boolean
+  def check_brackets(str) do
+    str
+    |> String.codepoints()
+    |> check([])
+  end
+
+  for {opener, closer} <- [{"{", "}"}, {"(", ")"}, {"[", "]"}, {"<", ">"}] do
+    defp check([unquote(opener) | tail], stack), do: check(tail, [unquote(opener) | stack])
+    defp check([unquote(closer) | tail], [unquote(opener) | stack]), do: check(tail, stack)
+    defp check([unquote(closer) | tail], stack), do: false
+  end
+
+  defp check([_head | tail], stack), do: check(tail, stack)
+  defp check([], stack), do: stack == []
+end
