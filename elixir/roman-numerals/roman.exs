79bb0227c1fbe6a2f250680a76c24abbab6cf12e
Christian Geuer-Pollmann <christian.geuer-pollmann@web.de>
2018-02-14 22:39:00 +0100

    .

--- elixir/roman-numerals/roman.exs
+++ elixir/roman-numerals/roman.exs
@@ -3,18 +3,25 @@ defmodule Roman do
   Convert the number to a roman number.
   """
   @spec numerals(pos_integer) :: String.t()
-  def numerals(number) when 0 <= number and number < 10,
-    do: ["", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"] |> Enum.at(number)
+  def numerals(number) do
+    tnds(number) <> huns(number) <> tens(number) <> ones(number)
+  end
 
-  def numerals(number) when 10 <= number and number < 100,
-    do:
-      (["", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"] |> Enum.at(div(number, 10))) <>
-        numerals(rem(number, 10))
+  defp ones(n), do: n |> form(1, "I", "V", "X")
+  defp tens(n), do: n |> form(10, "X", "L", "C")
+  defp huns(n), do: n |> form(100, "C", "D", "M")
+  defp tnds(n), do: n |> form(1000, "M", "?", "?")
 
-  def numerals(number) when 100 <= number and number < 1000,
-    do:
-      (["", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"] |> Enum.at(div(number, 100))) <>
-        numerals(rem(number, 100))
+  defp form(n, place, one, five, ten), do: n |> div(place) |> rem(10) |> place_value(one, five, ten)
 
-  def numerals(number) when 1000 <= number, do: "M" <> numerals(number - 1000)
+  defp place_value(0, _one, _five, _ten), do: ""
+  defp place_value(1, one, _five, _ten), do: one
+  defp place_value(2, one, _five, _ten), do: one <> one
+  defp place_value(3, one, _five, _ten), do: one <> one <> one
+  defp place_value(4, one, five, _ten), do: one <> five
+  defp place_value(5, _one, five, _ten), do: five
+  defp place_value(6, one, five, _ten), do: five <> one
+  defp place_value(7, one, five, _ten), do: five <> one <> one
+  defp place_value(8, one, five, _ten), do: five <> one <> one <> one
+  defp place_value(9, one, _five, ten), do: one <> ten
 end
