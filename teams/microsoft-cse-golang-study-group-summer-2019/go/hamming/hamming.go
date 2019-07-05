f79acce8d1f8a40b18bc29f87381296245ac5d9d
Christian Geuer-Pollmann <chgeuer@microsoft.com>
2019-07-05 10:58:28 +0200

    Copied previous solution over

--- teams/microsoft-cse-golang-study-group-summer-2019/go/hamming/hamming.go
+++ teams/microsoft-cse-golang-study-group-summer-2019/go/hamming/hamming.go
@@ -1,4 +1,23 @@
 package hamming
 
+import (
+	"errors"
+)
+
+// Distance computes the Hamming distance between two DNA sequences.
 func Distance(a, b string) (int, error) {
+	if len(a) != len(b) {
+		return -1, errors.New("DNA sequences don't have same size")
+	}
+
+	ra := []rune(a)
+	rb := []rune(b)
+	hammingWeight := 0
+	for i := 0; i < len(ra); i++ {
+		if ra[i] != rb[i] {
+			hammingWeight++
+		}
+	}
+
+	return hammingWeight, nil
 }
