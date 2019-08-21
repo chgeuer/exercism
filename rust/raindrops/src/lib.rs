44efe7d3438255b77d530505599a8a48b3eda118
Christian Geuer-Pollmann <chgeuer@microsoft.com>
2019-08-21 12:12:26 +0200

    .

--- rust/raindrops/src/lib.rs
+++ rust/raindrops/src/lib.rs
@@ -1,10 +1,14 @@
 pub fn raindrops(n: u32) -> String {
-    let mut plingplangplong = String::with_capacity(15);
+    let div_by = |x| n % x == 0;
+    let n2str = |x, v| if div_by(x) { v } else { "" };
 
-    if n % 3 == 0 { plingplangplong.push_str("Pling"); }
-    if n % 5 == 0 { plingplangplong.push_str("Plang"); }
-    if n % 7 == 0 { plingplangplong.push_str("Plong"); }
-    
+    let plingplangplong = &[ 
+        n2str(3, "Pling"), 
+        n2str(5, "Plang"), 
+        n2str(7, "Plong"),
+    ];
+    let plingplangplong = plingplangplong.join("");
+
     match plingplangplong.as_str() {
         "" => n.to_string(),
         _ => plingplangplong,
