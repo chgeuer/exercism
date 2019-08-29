72d240095bac50d4d7a9f1b7608c5431269b7c76
Christian Geuer-Pollmann <chgeuer@microsoft.com>
2019-08-29 14:33:46 +0200

    .

--- rust/matching-brackets/src/lib.rs
+++ rust/matching-brackets/src/lib.rs
@@ -1,24 +1,18 @@
 pub fn brackets_are_balanced(input: &str) -> bool {
+    let opener = |c: char| match c {
+        ')' => Some('('),
+        ']' => Some('['),
+        '}' => Some('{'),
+        _ => panic!("Wrong input {}", c),
+    };
+
     let mut stack = String::with_capacity(input.len() / 2);
     for c in input.chars() {
         match c {
             '(' | '[' | '{' => stack.push(c),
-            ')' => {
-                match stack.pop() {
-                    Some('(') => (),
-                    _ => return false,
-                }
-            },
-            ']' => {
-                match stack.pop() {
-                    Some('[') => (),
-                    _ => return false,
-                }
-            },
-            '}' => {
-                match stack.pop() {
-                    Some('{') => (),
-                    _ => return false,
+            ')' | ']' | '}' => {
+                if stack.pop() != opener(c) {
+                    return false;
                 }
             },
             _ => (),
