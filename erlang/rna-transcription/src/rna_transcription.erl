64b3ec11bc7ad2385ac9a603546d59884687cc93
Christian Geuer-Pollmann <chgeuer@microsoft.com>
2020-07-08 16:22:32 +0200

    .

--- erlang/rna-transcription/src/rna_transcription.erl
+++ erlang/rna-transcription/src/rna_transcription.erl
@@ -1,10 +1,11 @@
 -module(rna_transcription).
 -export([to_rna/1]).
 
-to_rna([]) -> [];
-to_rna([X | T]) -> [map(X) | to_rna(T)].
+to_rna(Strand) -> 
+	lists:map(fun map/1, Strand).
 
 map($G) -> $C;
 map($C) -> $G;
 map($T) -> $A;
 map($A) -> $U.
+
