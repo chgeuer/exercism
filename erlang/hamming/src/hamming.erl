d729c182eeb6efb98e89fd0fd3d1aa14cc7215f8
Christian Geuer-Pollmann <chgeuer@microsoft.com>
2020-07-09 13:54:55 +0200

    .

--- erlang/hamming/src/hamming.erl
+++ erlang/hamming/src/hamming.erl
@@ -1,11 +1,12 @@
 -module(hamming).
 -export([distance/2]).
+-define(LENGTH_ERROR,{ error, "left and right strands must be of equal length" }).
 
 distance(Strand1, Strand2) -> distance(Strand1, Strand2, 0).
 
 distance([], [], Count) -> Count;
 distance([H|T1], [H|T2], Count) -> distance(T1, T2, Count);
-distance([H1|T1], [H2|T2], Count) when H1 /= H2 -> distance(T1, T2, Count + 1);
-distance([], [_], _) -> { error, "left and right strands must be of equal length" };
-distance([_], [], _) -> { error, "left and right strands must be of equal length" }.
+distance([_|T1], [_|T2], Count) -> distance(T1, T2, Count + 1);
+distance([], [_], _) -> ?LENGTH_ERROR;
+distance([_], [], _) -> ?LENGTH_ERROR.
 
