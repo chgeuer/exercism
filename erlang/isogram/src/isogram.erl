77e661493e0a5feb52c07b190de50b2ca8dacd20
Christian Geuer-Pollmann <chgeuer@microsoft.com>
2020-07-23 18:23:12 +0200

    .

--- erlang/isogram/src/isogram.erl
+++ erlang/isogram/src/isogram.erl
@@ -2,5 +2,15 @@
 
 -export([is_isogram/1]).
 
+-define(IS_CHAR(C), (C >= $A), (C =< $Z)).
 
-is_isogram(_Phrase) -> undefined.
+is_isogram(Phrase) -> is_isogram(string:uppercase(Phrase), maps:new()).
+
+is_isogram([], _) -> true ;
+is_isogram([Char|Tail], Map) when ?IS_CHAR(Char) -> 
+	case maps:is_key(Char, Map) of 
+		true -> false;
+		false -> is_isogram(Tail, Map#{ Char => true })
+	end;
+is_isogram([_NonChar|Tail], Map) -> is_isogram(Tail, Map).
+
