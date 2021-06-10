1e0a6c48ebe6116fbf90ba78a15c5bbefece1893
Dr. Christian Geuer-Pollmann <chgeuer@microsoft.com>
2021-06-10 08:10:29 +0200

    Right

---
+++ erlang/queen-attack/src/queen_attack.erl
@@ -0,0 +1,7 @@
+-module(queen_attack).
+
+-export([can_attack/2]).
+
+can_attack({X, _}, {X, _}) -> true;
+can_attack({_, Y}, {_, Y}) -> true;
+can_attack({X1, Y1}, {X2, Y2}) -> abs(X1 - X2) =:= abs(Y1 - Y2).
